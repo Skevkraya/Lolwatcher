033bdd3712c05141fc4bcd66a51fe12c189e5fe4
UmmomValkyrie <pcervi@myges.fr>
2025-11-28 16:45:43 +0100

    Modif pour docker compose - pcervi

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,15 +1,18 @@
-// lancer backend : npx nodemon src/index.ts
-// Prisma studio : npx prisma studio 
-
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 
 dotenv.config();
 const app = express();
-app.use(cors());
-app.use(express.json());
 
+app.use(
+  cors({
+    origin: ["http://localhost:3000"],
+    credentials: true, // si tu utilises des cookies ou tokens
+  })
+);
+
+app.use(express.json());
 
 // Routes
 import authRoutes from "./routes/auth";
@@ -17,12 +20,15 @@ import accountRoutes from "./routes/accounts";
 import matchRoutes from "./routes/matches";
 import userRoutes from "./routes/user";
 
-
 app.use("/auth", authRoutes);
 app.use("/accounts", accountRoutes);
 app.use("/matches", matchRoutes);
 app.use("/user", userRoutes);
 
+app.get("/health", (_, res) => res.status(200).json({ status: "ok" }));
 
 const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => console.log(`API running on http://localhost:${PORT}`));
+app.listen(PORT, () =>
+  console.log(`API running on http://localhost:${PORT}`)
+);
+
